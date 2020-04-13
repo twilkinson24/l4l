f4c16e4fbd38ca190486365bb65944970658cee6
Taylor <taylorwteaches@gmail.com>
2020-04-13 09:54:50 -0400

    fixing padding and margins in header

--- sidebar-templates/sidebar-headeraddressphone.php
+++ sidebar-templates/sidebar-headeraddressphone.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) || exit;
 <?php if ( is_active_sidebar( 'custom-header-address-widget' ) ) : ?>
 	<!-- ******************* Header Address Area ******************* -->
     <div class="phone-wrap d-none d-lg-flex">
-        <i class="fas fa-map-marker"></i>
+        <i class="fas fa-map-marker mx-3"></i>
         <address>
             <?php dynamic_sidebar( 'custom-header-address-widget' ); ?>
         </address>
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) || exit;
 <?php if ( is_active_sidebar( 'custom-header-phone-widget' ) ) : ?>
     <!-- ******************* Header Phone Area ******************* -->
     <div class="phone-wrap d-none d-lg-flex">
-        <i class="fas fa-phone"></i>
+        <i class="fas fa-phone mx-3"></i>
         <?php dynamic_sidebar( 'custom-header-phone-widget' ); ?>
     </div>
 <?php endif; ?>
